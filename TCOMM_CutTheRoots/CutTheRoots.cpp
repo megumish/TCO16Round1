23e9af5e13ccaeae8e51929ac6929068390ddfaf
Kawada Keishi <Kk-shli@outlook.jp>
2016-04-09 01:04:07 +0900

    random approach solution. It can't work well.

--- TCOMM_CutTheRoots/CutTheRoots.cpp
+++ TCOMM_CutTheRoots/CutTheRoots.cpp
@@ -4,6 +4,8 @@
 #include <map>
 #include <sstream>
 #include <vector>
+#include <tuple>
+#include <random>
 
 using namespace std;
 
@@ -12,18 +14,36 @@ public:
     vector<int> makeCuts(int NP, vector<int> points, vector<int> roots) {
         // first NP points give coordinates of the bases of the plants, written as x, y
         // get x-coordinates of the bases, sort them, make cuts along y-axis to separate x-coordinates
-        vector<int> xs(NP);
+        vector<pair<int,int>> xys(NP);
         for (int i = 0; i < NP; ++i) {
-            xs[i] = points[2 * i];
+            xys[i].first  = points[2 * i];
+            xys[i].second = points[2 * i + 1];
         }
-        sort(xs.begin(), xs.end());
+        sort(xys.begin(), xys.end());
+        
+        double eps = 1e-1;
+        random_device seed_gen;
+        mt19937 engine(seed_gen());
+        uniform_real_distribution<double> distRatio(0+eps, 1-eps);
         vector<int> ret(4 * (NP - 1));
         for (int i = 0; i < NP - 1; ++i) {
-            int x = (xs[i]+xs[i+1])/2;
-            ret[4 * i] = x;
-            ret[4 * i + 1] = 1;
-            ret[4 * i + 2] = x;
-            ret[4 * i + 3] = 2;
+            double ratio = distRatio(engine);
+            int a = ratio*xys[i].first + (1.0-ratio)*xys[i+1].first;
+            int b = ratio*xys[i].second + (1.0-ratio)*xys[i+1].second;
+            normal_distribution<double> distK(b, 1.0);
+            int k = distK(engine);
+            ret[4 * i] = a;
+            ret[4 * i + 1] = b;
+            if (k < 0 || k > 1024)
+            {
+                ret[4 * i + 2] = a;
+                ret[4 * i + 3] = b + 1;
+            }
+            else
+            {
+                ret[4 * i + 2] = a + 1;
+                ret[4 + i + 3] = k;
+            }
         }
         return ret;
     }
--- TCOMM_CutTheRoots/start.bat
+++ TCOMM_CutTheRoots/start.bat
@@ -1,2 +1,2 @@
-java -jar Tester.jar -exec "..\Release\TCOMM_CutTheRoots.exe" -seed 3 -vis -save output.png
+java -jar Tester.jar -exec "..\Release\TCOMM_CutTheRoots.exe" -seed 3843 -vis -save random.png
 @pause
